b49af0a7744163cab879c13669dfe5b5c7ae2031
Attila <attilakiss323@gmail.com>
2021-08-02 10:01:29 +0200

    [CHORE] Add RELEASE title option (#29)
    * [CHORE] Add RELEASE title option
    
    * 1.11.9

--- dangerjs/title.js
+++ dangerjs/title.js
@@ -13,13 +13,14 @@ const titleError = [
   '- `[CHORE]` for any other tasks\n',
   '- `[SETUP]` for any setup-related\n',
   '- `[DEBUG]` for debugging purposes\n',
+  '- `[RELEASE]` for mobile-app release PRs\n',
   'Automatically:\n',
   '- `[BACK-PROPAGATION]` for important changes which also required in other branches (do not change this title)\n',
   '- `Bump ...` for dependabot (do not change this title)\n\n',
   `![img](${getRandomImage('wrongTitle')}, 'Oops')\n`
 ]
 
-const genericPrefixes = ['[HOTFIX]', '[BUGFIX]', '[CHORE]', '[SETUP]', '[DEBUG]', '[BACK-PROPAGATION]']
+const genericPrefixes = ['[HOTFIX]', '[BUGFIX]', '[CHORE]', '[SETUP]', '[DEBUG]', '[BACK-PROPAGATION]', '[RELEASE]']
 const dependabotPrefix = 'Bump'
 const teamSpecificPrefixes = /\[(PD|IES|QA|SI)-\d+\].*/g // e.g. [PD-1234], [IES-1234], [QA-1234]
 
